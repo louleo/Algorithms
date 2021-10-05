da52e2e22de4eb6b9d8c6fc9991765520e134f91
louleo <louleolin@gmail.com>
2021-10-05 13:36:29 +1100

    Update 1005 - 01 - C#

---
+++ c-sharp/Algorithms/LeetCodeTests/RotateImageTests.cs
@@ -0,0 +1,31 @@
+using FluentAssertions;
+using LeetCode;
+using Xunit;
+
+namespace LeetCodeTests
+{
+    public class RotateImageTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            int[][] matrix = new[]
+            {
+                new[] {1, 2, 3},
+                new[] {4, 5, 6},
+                new[] {7, 8, 9}
+            };
+            int[][] output = new[]
+            {
+                new[] {7, 4, 1},
+                new[] {8, 5, 2},
+                new[] {9, 6, 3}
+            };
+
+            RotateImageSolution solution = new RotateImageSolution();
+            solution.Rotate(matrix);
+
+            matrix.Should().Equal(output);
+        }
+    }
+}
