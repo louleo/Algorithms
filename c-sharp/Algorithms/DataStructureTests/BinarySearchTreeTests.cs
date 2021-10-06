f82b43c68c7b3c45372cf009f6850f92edfbcc4e
louleo <louleolin@gmail.com>
2021-10-06 12:20:19 +1100

    Remove Binary Tree Deletion Tests.

--- c-sharp/Algorithms/DataStructureTests/BinarySearchTreeTests.cs
+++ c-sharp/Algorithms/DataStructureTests/BinarySearchTreeTests.cs
@@ -8,17 +8,6 @@ namespace DataStructureTests
 {
     public class BinarySearchTreeTests
     {
-        [Fact]
-        public void Test_BinarySearchTree_Deletion()
-        {
-            int[] arr = new []{7,3,4,5,1,2,10,8,9,12,11,13};
-            BinarySearchTree bst = new BinarySearchTree(arr);
-            List<int> list = arr.ToList();
-            bst.Delete(10);
-            list.Remove(10);
-            list.Sort();
-            List<int> result = (List<int>) bst.ToList();
-            result.Should().Equal(list);
-        }
+
     }
 }
