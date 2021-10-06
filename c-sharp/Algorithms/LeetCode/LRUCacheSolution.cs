800ab546c491094d5565470b1bf52924ddc5748c
louleo <louleolin@gmail.com>
2021-10-06 11:59:05 +1100

    C# - Leetcode 146 solution and tests.

--- c-sharp/Algorithms/LeetCode/LRUCacheSolution.cs
+++ c-sharp/Algorithms/LeetCode/LRUCacheSolution.cs
@@ -1,167 +1,100 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography;
+using System.Transactions;
 
 namespace LeetCode
 {
 
     public class LinkedListNode
     {
-        private LinkedListNode _prev;
-        private LinkedListNode _next;
-        private int _value;
-
-        public LinkedListNode(int value)
-        {
-            _value = value;
-        }
-
-        public void AddPrev(LinkedListNode prev)
-        {
-            _prev = prev;
-        }
-
-        public void AddNext(LinkedListNode next)
-        {
-            _next = next;
-        }
-
-        public LinkedListNode GetNext()
-        {
-            return _next;
-        }
-
-        public LinkedListNode GetPrev()
-        {
-            return _prev;
-        }
-    }
-
-
-    public class LinkedList
-    {
-        private LinkedListNode _head = null;
-        private LinkedListNode _tail = null;
-        private int _capacity;
-        private int _count;
-
-        public LinkedList(int capacity)
-        {
-            _capacity = capacity;
-            _count = 0;
-        }
-
-        public bool Append(LinkedListNode nodeToAdd)
-        {
-            if (this._count == this._count)
-            {
-                return false;
-            }
-
-            if (this._head is null)
-            {
-                this._head = nodeToAdd;
-                this._tail = nodeToAdd;
-                this._count = 1;
-                return true;
-            }
-
-            nodeToAdd.AddPrev(this._tail);
-            this._tail.AddNext(nodeToAdd);
-            this._tail = nodeToAdd;
-            _count += 1;
-            return true;
-        }
-
-        public bool RemoveFirst()
-        {
-            if (this._head is null)
-            {
-                return false;
-            }
-
-            if (this._count == 1)
-            {
-                this._head = null;
-                this._tail = null;
-                this._count = 0;
-            }
-
-            LinkedListNode nodeToRemove = this._head;
-            this._head = this._head.GetNext();
-            this._head.AddPrev(null);
-            nodeToRemove.AddNext(null);
-            _count--;
-            return true;
-        }
-    }
-
-    public class KeyValuePair
-    {
-        public int Key;
         public int Value;
+        public int Key;
+        public LinkedListNode PrevNode;
+        public LinkedListNode NextNode;
 
-        public KeyValuePair(int key, int value)
+        public LinkedListNode(int key, int value)
         {
             Key = key;
             Value = value;
         }
 
-        public override bool Equals(KeyValuePair? keyValuePair)
+        public LinkedListNode()
         {
-            return base.Equals(obj);
+            
         }
     }
     
-    public class LRUCacheSolution
+    public class LRUCache
     {
-        private Dictionary<int, LinkedListNode<KeyValuePair>> _cache;
-        private LinkedList<KeyValuePair> _list;
+        private Dictionary<int, LinkedListNode> _cache;
         private int _capacity;
+        private LinkedListNode _head;
+        private LinkedListNode _tail;
 
-        public LRUCacheSolution(int capacity)
+        public LRUCache(int capacity)
         {
             this._capacity = capacity;
-            this._list = new LinkedList<KeyValuePair>();
-            this._cache = new Dictionary<int, LinkedListNode<KeyValuePair>>();
+            this._cache = new Dictionary<int, LinkedListNode>();
+            this._head = new LinkedListNode();
+            this._tail = new LinkedListNode();
+            _tail.PrevNode = _head;
+            _head.NextNode = _tail;
         }
 
         public int Get(int key)
         {
-            LinkedListNode<KeyValuePair> node;
+            LinkedListNode node;
             if (_cache.TryGetValue(key, out node))
             {
-                ReorderList(node);
-                return node.Value.Value;
+                MoveNodeToLast(node);
+                return node.Value;
             }
             return -1;
         }
 
         public void Put(int key, int value)
         {
-            LinkedListNode<KeyValuePair> node = new LinkedListNode<KeyValuePair>(new KeyValuePair(key, value));
+            LinkedListNode node = new LinkedListNode(key, value);
             if (_cache.TryAdd(key, node))
             {
+                AddNodeToLast(node);
+
                 if (_cache.Count > _capacity)
                 {
-                    _cache.Remove(_list.First.Value.Key);
-                    _list.RemoveFirst();
+                    _cache.Remove(_head.NextNode.Key);
+                    RemoveNode(_head.NextNode);
                 }
-                
+
             }
             else
             {
-                _cache[key] = node;
-                ReorderList(node);
+                _cache[key].Value = value;
+                MoveNodeToLast(_cache[key]);
             }
         } 
 
 
-        private void ReorderList(LinkedListNode<KeyValuePair> node)
+        private void RemoveNode(LinkedListNode node)
         {
-            
-            _list.AddLast(node);
+            node.PrevNode.NextNode = node.NextNode;
+            node.NextNode.PrevNode = node.PrevNode;
         }
 
+
+        private void AddNodeToLast(LinkedListNode node)
+        {
+            node.NextNode = _tail;
+            node.PrevNode = _tail.PrevNode;
+            _tail.PrevNode.NextNode = node;
+            _tail.PrevNode = node;
+        }
+
+        private void MoveNodeToLast(LinkedListNode node)
+        {
+            RemoveNode(node);
+            AddNodeToLast(node);
+        }
+        
     }
 }
